d98355e73075f6911c5c296a1101d5948a20c4bc
Lukas <lukas.tegethoff2@gmail.com>
2018-06-05 01:03:05 +0200

    SQL-Scripts for creation of DB and some test data.

---
+++ misc/dataImport/SQLDOTInsertDOTScript.sql
@@ -0,0 +1,17 @@
+insert into Employer (Name,Username,Password,Description) values ('InfoAG','BestITBOIS','123456789','Test Firm Number 1');
+insert into Employer (Name,Username,Password,Description) values ('Paderbyte','OverMinimumWage','123456789','Test Firm Number 2');
+insert into Employer (Name,Username,Password,Description) values ('REAL','UnderMinimumWage','123456789','Test Firm Number 3');
+insert into Employer (Name,Username,Password,Description) values ('LIDL','20DollarJob','123456789','Test Firm Number 4');
+insert into Employer (Name,Username,Password,Description) values ('ORGA','WeDontHire','123456789','Test Firm Number 5');
+
+insert into User(Lastname,Forename,Description,Username,Password) values ('Mustermann','Max','Test User 1','MadMax','Test123!');
+insert into User(Lastname,Forename,Description,Username,Password) values ('Swanson','Ron','Test User 2','Creativity','Test123!');
+insert into User(Lastname,Forename,Description,Username,Password) values ('Johnson','Chad','Test User 3','theRealChad','Test123!');
+insert into User(Lastname,Forename,Description,Username,Password) values ('Miller','Steve','Test User 4','ITGOD','Test123!');
+insert into User(Lastname,Forename,Description,Username,Password) values ('Parker','Peter','Test User 5','MiderSpan','Test123!');
+
+insert into Skill(Name,Description) values ('Coffee','The Ability to make Coffee');
+insert into Skill(Name,Description) values ('SQL','Writing of Scripts in Windows Server');
+insert into Skill(Name,Description) values ('Windows Server 2012','Knowledge of Windows Server 2012');
+insert into Skill(Name,Description) values ('Design','Certain Knowledge about Designing Stuff');
+insert into Skill(Name,Description) values ('JAVA','Programming with JAVA');
